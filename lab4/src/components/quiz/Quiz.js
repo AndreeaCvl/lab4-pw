6f448675ccf03194fd23c9eba7a51f927f39597c
AndreeaCvl <44116789+AndreeaCvl@users.noreply.github.com>
2023-05-23 00:28:24 +0300

    quiz answer

--- lab4/src/components/quiz/Quiz.js
+++ lab4/src/components/quiz/Quiz.js
@@ -2,70 +2,81 @@ import { useLocation } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 
 import { getQuizByID } from "../../requests";
-import { submitQuizAnswers} from "../../requests";
+import { submitQuizAnswers } from "../../requests";
 
 function Quiz() {
+  const [questions, setQuestions] = useState([]);
+  const [quizTitle, setQuizTitle] = useState("");
+  const [quizId, setQuizId] = useState(0);
+  const [userId, setUserId] = useState(0);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-    const [questions, setQuestions] = useState([]);
-    const [quizTitle, setQuizTitle] = useState("");
-    const [quizId, setQuizId] = useState(0);
-    const [userId, setUserId] = useState(0);
-    const [selectedAnswers, setSelectedAnswers] = useState({});
+  const location = useLocation();
 
-    const location = useLocation();
+  useEffect(() => {
+    getQuizByID(location.state.id).then((response) => {
+      console.log(response.data.questions);
+      setQuestions(response.data.questions);
+      setQuizTitle(response.data.title);
+      setQuizId(response.data.id);
+      setUserId(location.state.userId);
+    });
+  }, []);
 
-    useEffect(() => {
-        getQuizByID(location.state.id).then(response=>{
-            console.log(response.data.questions)
-            setQuestions(response.data.questions)
-            setQuizTitle(response.data.title)
-            setQuizId(response.data.id)
-            setUserId(location.state.userId)
-        })
-    }, []);
-    
-    const handleAnswerSelection = (questionId, answer) => {
-        setSelectedAnswers(prevSelectedAnswers => ({
-          ...prevSelectedAnswers,
-          [questionId]: answer
-        }));
-        console.log(selectedAnswers)
-        
-        const postAnswerRequest = {questionId, answer, userId, quizId};
-        
-        submitQuizAnswers(postAnswerRequest).then(response=>{
-            console.log(response);
-        });
-    };
-    
-    return (
-        <div>
-            <div>{quizTitle}</div>
-            {questions.map(question => {
-            return (
-            <div className="questionElement" key={question.id}>
-                <div className="question">{question.question}</div>
+  const handleAnswerSelection = (questionId, answer) => {
+    setSelectedAnswers((prevSelectedAnswers) => ({
+      ...prevSelectedAnswers,
+      [questionId]: answer,
+    }));
+    console.log(selectedAnswers);
 
-                <div className="answers">
-                    {question.answers.map((answer, index) => (
-                        <label className="answerElement" key={index}>
-                        <input
-                            type="radio"
-                            name={`question_${question.id}`}
-                            value={answer}
-                            checked={selectedAnswers[question.id] === answer}
-                            onChange={() => handleAnswerSelection(question.id, answer)}            
-                        />
-                        {answer}
-                        </label>
-                    ))}
-                </div>
+    const postAnswerRequest = { questionId, answer, userId, quizId };
 
-                <div className="quizQuestionCount">{question.questions_count}</div>
-            </div>
-            )})}
+    submitQuizAnswers(postAnswerRequest).then((response) => {
+      console.log(response);
+    });
+
+    // Move to the next question
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  if (currentQuestionIndex >= questions.length) {
+    // All questions have been answered
+    return <div>Quiz completed!</div>;
+  }
+
+  const currentQuestion = questions[currentQuestionIndex];
+
+  return (
+    <div>
+      <div>{quizTitle}</div>
+      <div className="questionElement" key={currentQuestion.id}>
+        <div className="question">{currentQuestion.question}</div>
+
+        <div className="answers">
+          {currentQuestion.answers.map((answer, index) => (
+            <label className="answerElement" key={index}>
+              <input
+                type="radio"
+                name={`question_${currentQuestion.id}`}
+                value={answer}
+                checked={selectedAnswers[currentQuestion.id] === answer}
+                onChange={() =>
+                  handleAnswerSelection(currentQuestion.id, answer)
+                }
+              />
+              {answer}
+            </label>
+          ))}
         </div>
-    )
+
+        <div className="quizQuestionCount">
+          {currentQuestionIndex + 1}/{questions.length}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Quiz
+export default Quiz;
